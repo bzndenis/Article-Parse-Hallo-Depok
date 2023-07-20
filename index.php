1e037c11332de6c9c8e5b04647584c1b80184b09
bzndenis <bzndenis@gmail.com>
2023-07-20 21:30:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +9,7 @@
         body {
             background-color: #2C2F33;
             color: #748B9C;
+            margin-bottom: 80px; /* Menambahkan margin-bottom untuk memberikan ruang pada footer */
         }
         
         a {
@@ -19,9 +21,21 @@
         }
         
         #footer {
-            margin-top: 50px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            height: 80px; /* Menetapkan tinggi footer */
             text-align: center;
             color: #748B9C;
+            background-color: #2C2F33;
+        }
+
+        @media (max-width: 767px) {
+            /* Mengatur posisi footer untuk layar kecil */
+            #footer {
+                position: relative;
+                margin-top: 20px;
+            }
         }
     </style>
     <script src="https://cdn.tiny.cloud/1/vmzgpqebnq23hnpw7i0ubynejm8aur0uhmp7benl2c6k6ln6/tinymce/5/tinymce.min.js"></script>
@@ -37,6 +51,7 @@
 
         function copyToClipboard() {
             var articleText = document.getElementById("parsed-article").innerText;
+            articleText = articleText.replace(/\*/g, ''); // Menghapus karakter '*'
             navigator.clipboard.writeText(articleText).then(function() {
                 alert("Artikel berhasil disalin!");
             }, function() {
@@ -71,11 +86,15 @@
                 <textarea id="article" name="article" class="form-control"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button><br>
-        </form>
+        </form><br>
 
         <?php
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $article = $_POST['article'];
+            
+            // Hapus karakter '*' dalam artikel
+            $article = str_replace('*', '', $article);
+            //$article = str_replace('"', '', $article);
 
             // Tidy the paragraphs
             $article = preg_replace('/<p>\s*(.*?)\s*<\/p>/', '<p>$1</p>', $article, -1);
@@ -95,12 +114,14 @@
         <button id="copyButton" class="btn btn-primary" onclick="copyToClipboard()">Salin Artikel Hanya Text</button>
         <button id="copyAsHyperlinkButton" class="btn btn-info" onclick="copyAsHyperlink()">Salin Artikel dengan Hyperlink</button>
         <textarea id="textToCopy" style="display: none;"><?php echo strip_tags($article); ?></textarea>
-        
-        <div id="footer">
-            <p>	
-Made with <span id="icon-love" class="et-pb-icon et-waypoint et_pb_animation_top et-animated">&#10084;</span> by <a href="http://koys.my.id" target="_blank">Bekoy</a>
-</p>
-        </div>
     </div>
+    
+    <div id="footer" class="fixed-bottom">
+        <p>	
+            Made with <span id="icon-love" class="et-pb-icon et-waypoint et_pb_animation_top et-animated">&#10084;</span> by <a href="http://koys.my.id" target="_blank">Bekoy</a>
+        </p>
+    </div>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
